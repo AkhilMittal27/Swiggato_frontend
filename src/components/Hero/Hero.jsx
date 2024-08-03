2be0080fe862e250ce19ebd55fe7914339562b95
Akhil Mittal <akhilmittal755@gmail.com>
2024-08-03 15:24:20 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Delivery from '../Delivery/Delivery'
+import Allcards from '../Allcards/Allcards'
+
+
+const Hero = () => {
+  return (
+    <div>
+       <Delivery/>
+       <Allcards/>
+    </div>
+  )
+}
+
+export default Hero
---
+++ src/helper.js
@@ -0,0 +1,2 @@
+const apiUrl = import.meta.env.VITE_API_URL;
+export const BASE_URL = apiUrl;
